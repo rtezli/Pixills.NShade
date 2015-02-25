26e349aef1342b3e334f05a2df5b484b8c4b7b34
Robert Tezli <robert@pixills.com>
2015-02-25 17:28:58 +0100

    altered shader

--- NShadeCore/PhongPixelShader.hlsl
+++ NShadeCore/PhongPixelShader.hlsl
@@ -11,11 +11,9 @@ struct PixelShaderInput
 
 float4 main(PixelShaderInput input) : SV_TARGET
 { 
-	float4 diffuse		= dot(input.light, input.normal);
-
-	float  specularity	= 32.0f;
-	float  specConst	= (specularity + 2.0f) / (2.0f * 3.14f);
-	float4 specular		= specConst * dot(input.eye, diffuse);
-
-	return input.color  * (diffuse + specular);
+	float  specularPower  = 0.1f;
+	float  lightIntensity = 0.1f;//input.light.w;
+	float4 diffuse		  = lightIntensity * dot(input.light, input.normal);
+	float4 specular		  = lightIntensity * dot(input.light, diffuse) * specularPower;
+	return input.color    * (diffuse + specular);
 }
