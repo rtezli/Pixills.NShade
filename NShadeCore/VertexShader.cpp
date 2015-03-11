c32a914f003eca304c8a15eae5300bd9981e13ee
Robert Tezli <robert@pixills.com>
2015-03-11 17:31:40 +0100

    updated buffer code

--- NShadeCore/VertexShader.cpp
+++ NShadeCore/VertexShader.cpp
@@ -19,8 +19,15 @@ VOID VertexShader::Compile(CHAR *file, ShaderVersion version)
 
 VOID VertexShader::CreateBuffers(vector<NVertex> *vertices, vector<UINT> *indices)
 {
+	/* Vertex Buffer */
+
+	NVertex* vertexArr;
+	UINT verticesSize = vertices->size();
+	vertexArr = (NVertex*)malloc(verticesSize * sizeof(NVertex));
+	copy(vertices->begin(), vertices->end(), vertexArr);
+
 	D3D11_BUFFER_DESC  vertexBufferDesc;
-	vertexBufferDesc.ByteWidth = sizeof(NVertex) * vertices->size();
+	vertexBufferDesc.ByteWidth = sizeof(NVertex) * verticesSize;
 	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
 	vertexBufferDesc.CPUAccessFlags = 0;
 	vertexBufferDesc.MiscFlags = 0;
@@ -28,7 +35,7 @@ VOID VertexShader::CreateBuffers(vector<NVertex> *vertices, vector<UINT> *indice
 	vertexBufferDesc.StructureByteStride = 0;
 
 	D3D11_SUBRESOURCE_DATA vertexBufferData;
-	vertexBufferData.pSysMem = &vertices[0];
+	vertexBufferData.pSysMem = vertexArr;
 	vertexBufferData.SysMemPitch = 0;
 	vertexBufferData.SysMemSlicePitch = 0;
 
@@ -36,15 +43,22 @@ VOID VertexShader::CreateBuffers(vector<NVertex> *vertices, vector<UINT> *indice
 	auto result = _Resources->Device->CreateBuffer(&vertexBufferDesc, &vertexBufferData, &vertexBuffer);
 	_VertexBuffer = shared_ptr<ID3D11Buffer>(vertexBuffer);
 
+	/* Index Buffer */
+
+	UINT* indexArr;
+	UINT indicesSize = indices->size();
+	indexArr = (unsigned int*)malloc(indicesSize * sizeof(UINT));
+	copy(indices->begin(), indices->end(), indexArr);
+
 	D3D11_BUFFER_DESC indexBufferDesc;
-	indexBufferDesc.ByteWidth = sizeof(UINT) * indices->size();
+	indexBufferDesc.ByteWidth = sizeof(UINT) * indicesSize;
 	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
 	indexBufferDesc.CPUAccessFlags = 0;
 	indexBufferDesc.MiscFlags = 0;
 	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
 
 	D3D11_SUBRESOURCE_DATA indexBufferData;
-	indexBufferData.pSysMem = &indices[0];
+	indexBufferData.pSysMem = indexArr;
 	indexBufferData.SysMemPitch = 0;
 	indexBufferData.SysMemSlicePitch = 0;
 	indexBufferDesc.StructureByteStride = 0;
