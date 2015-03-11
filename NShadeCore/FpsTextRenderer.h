f4efd927bf145cd82ce6ef9b847530ccdbcc4bc6
Robert Tezli <robert@pixills.com>
2015-03-11 22:01:09 +0100

    Added missing files

---
+++ NShadeCore/FpsTextRenderer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "includes.h"
+
+class FpsTextRenderer
+{
+public:
+	FpsTextRenderer(DeviceResources* pResources);
+	~FpsTextRenderer();
+	HRESULT Initialize();
+private:
+	ID3D11Device*			const	Device(){ return Resources()->Device; }
+	ID3D11DeviceContext*	const	DeviceContext(){ return Resources()->DeviceContext; }
+	DeviceResources*		const	Resources(){ return m_pDeviceResources; }
+	DeviceResources*				m_pDeviceResources;
+};
+
