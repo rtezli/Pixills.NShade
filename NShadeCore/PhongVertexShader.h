d30c8b7c2353b783e5b755e8d45bfc608cb4c0eb
Robert Tezli <robert@pixills.com>
2015-03-11 08:02:13 +0100

    updated shaders

--- NShadeCore/PhongVertexShader.h
+++ NShadeCore/PhongVertexShader.h
@@ -5,12 +5,6 @@
 
 namespace PhongShader
 {
-	struct ExtraData
-	{
-		XMFLOAT4 AmbientColorIntensity;
-		XMFLOAT4 LightPositionIntensity;
-	};
-
 	static const D3D11_INPUT_ELEMENT_DESC InputDescription[] =
 	{
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT,    0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // Vertex position
