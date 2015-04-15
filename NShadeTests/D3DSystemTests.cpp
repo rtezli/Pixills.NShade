1e34e438ed99dc83859187dc8f775c3ef43429fe
Robert Tezli <robert@pixills.com>
2015-04-16 00:53:41 +0200

    updated project files

--- NShadeTests/D3DSystemTests.cpp
+++ NShadeTests/D3DSystemTests.cpp
@@ -2,6 +2,8 @@
 #include "cppunittest.h"
 #include "..\NShadeCore\D3DSystem.h"
 
+#pragma comment(lib, "Microsoft.VisualStudio.TestTools.CppUnitTestFramework.lib")
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace NShadeTests
@@ -9,31 +11,31 @@ namespace NShadeTests
     TEST_CLASS(D3DSystemTests)
     {
     public:
-        TEST_METHOD(InitializeTest)
+        TEST_METHOD(D3DSystem_InitializeTest)
         {
             auto d3d = new D3DSystem();
             Assert::IsNotNull(d3d);
         }
 
-        TEST_METHOD(D3DConstructorTestTest)
+        TEST_METHOD(D3DSystem_D3DConstructorTestTest)
         {
             auto d3d = new D3DSystem();
             Assert::IsNotNull(d3d);
         }
 
-        TEST_METHOD(InitializeForWindowTest)
+        TEST_METHOD(D3DSystem_InitializeForWindowTest)
         {
             auto d3d = new D3DSystem();
             Assert::IsNotNull(d3d);
         }
 
-        TEST_METHOD(InitializeWithWindowTest)
+        TEST_METHOD(D3DSystem_InitializeWithWindowTest)
         {
             auto d3d = new D3DSystem();
             Assert::IsNotNull(d3d);
         }
 
-        TEST_METHOD(MessageHandlerTest)
+        TEST_METHOD(D3DSystem_MessageHandlerTest)
         {
             auto d3d = new D3DSystem();
             Assert::IsNotNull(d3d);
